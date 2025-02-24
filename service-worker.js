e7e080d72831104f5de12475c3f9a0b2aa8773df
navii-a <agarwal.navin85@gmail.com>
2025-02-23 22:58:37 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,10 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('my-songbook-cache').then(cache => {
       return cache.addAll([
-        '/songbook.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
+        'songbook.html',
+        'manifest.json',
+        'icon-192.png',
+        'icon-512.png'
       ]);
     })
   );
